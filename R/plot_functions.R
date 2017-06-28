f3b6c9f1e45c419b011d23d1e3ee4d22d68db9da
Loren C Lipsey <lipseylc@hotmail.com>
2017-06-28 10:08:51 -0400

    Added hovertips functions; not working yet

--- R/plot_functions.R
+++ R/plot_functions.R
@@ -106,60 +106,146 @@ money_labels <- function(axis_values){
     sig_digits))
 }
 
-#' Create a style for a tooltip wellPanel
+#' Create a tooltip for a ggplot2 graph in Shiny
 #'
-#' @param background_color A hex value, as quoted string, to form the
-#' background color of the tooltip
-#' @param preferred_side One of c("bottom_right", "top_right", "bottom_left",
-#' "top_left")
-#' @param flip_on_squish_h How close, in pixels, the mouse can get to the
-#' (horizontal) edge of the chart before the tooltip flips to the other
-#' side to give more space
-#' @param flip_on_squish_v How close, in pixels, the mouse can get to the
-#' (vertical) edge of the chart before the tooltip flips to the other
-#' side to give more space
 #'
-#' @return A formatted string to include for wellPanel's style argument
-#' within an output renderUI function for hover tips
+#' @param html_content The content to display in the tooltip.
+#' @param chart_data A data frame.  The same data used to create the chart.
+#' @param hover_object The Shiny input object containing the hover info.
+#' @param x_var The quoted name of the variable on the X axis. NEVER MIND
+#' YOU CAN DO THIS WITH hover$mapping$x
+#' @param y_var The quoted name of the variable on the Y axis. NEVER MIND
+#' YOU CAN DO THIS WITH hover$mapping$x
+#' @param chart_type One of c("line", "bar", "stacked bar", "area").
+#' Other types are not currently supported and will require manual tips.
+#' @param ... Additional arguments to customize the look of the tooltip;
+#' will be passed through to hover_style()
 #'
-#' @usage wellPanel(style = hover_style(), "tip here")
+#' @return A wellPanel-based on-hover tooltip.
 #'
 #' @examples
+#' \dontrun{
 #' output$hover_info <- renderUI({
-#'   if(is.null(input$plot_hover)){return()}
-#'   year <- round(input$plot_hover$x)
-#'   wellPanel(
-#'     style = hover_style(),
-#'     p(HTML(paste("hovering over year:", year))))
+#'   hover_tip(
+#'     html_content = p(html("hello world")),
+#'     chart_data = dataset(),
+#'     hover_object = input$plot_hover,
+#'     chart_type = "stacked bar")
 #' })
-#'
+#' }
 #' @export
 
+hover_tip <- function(
+  chart_data,
+  hover_object,
+  html_content,
+  x_var = "Fiscal.Year",
+  y_var = "Amount",
+  chart_type = "line",
+  ...
+  ){
+
+  if(is.null(hover_object$x)) return()
+  if(is.null(hover_object$y)) return()
+
+
+}
+
+#' Define the tooltip visual style for a tooltip on a ggplot2 graph
+#' in a Shiny app
+#'
+#' @param hover_object The Shiny input object containing the hover info.
+#' @param background_color A hex value, as quoted string, to form the
+#' background color of the tooltip
+#' @param alpha A number between 0 and 1, to set the opacity of the
+#' tooltip background
+#' @param preferred_side One of c("bottom_right", "top_right", "bottom_left",
+#' "top_left")
+#' @param minimum_h The minimum horizonal width, in pixels, of the tooltip
+#' panel.  The panel will "squish" to be smaller as the cursor approaches
+#' the edge of the plotting area, but will not squish smaller than minimum_h.
+#' @param minimum_v The minimum vertical width, in pixels, of the tooltip
+#' panel.  The panel will "squish" to be smaller as the cursor approaches
+#' the edge of the plotting area, but will not squish smaller than minimum_v.
+#'
+#' @return A wellPanel-based on-hover tooltip
+#'
+#' @usage wellPanel(style = hover_style(input$plot_hover), "tip here")
+#'
+#' @examples
+#'
+#'
+
 hover_style <- function(
+  hover_object,
   background_color = "#f5f5f5",
+  alpha = 0.85,
   preferred_side = "bottom_right",
-  flip_on_squish_h = 100,
-  flip_on_squish_v = 100){
+  minimum_h = 100,
+  minimum_v = 100
+  ){
 
-
-  left_pct <- (hover$x - hover$domain$left) /
-    (hover$domain$right - hover$domain$left)
-  top_pct <- (hover$domain$top - hover$y) /
-    (hover$domain$top - hover$domain$bottom)
+  hover <- hover_object
+  rgb <- col2rgb(background_color)[,1]
+  if(alpha > 1 | alpha < 0) stop("alpha must be between 0 and 1")
 
   # calculate distance from left and bottom side of the picture in pixels
-  left_px <- hover$range$left + left_pct *
-    (hover$range$right - hover$range$left)
-  top_px <- hover$range$top + top_pct *
-    (hover$range$bottom - hover$range$top)
+  left_px <- hover$range$left +
+    (((hover$x - hover$domain$left) /
+    (hover$domain$right - hover$domain$left)) *
+    (hover$range$right - hover$range$left))
+  top_px <- hover$range$top +
+    (((hover$domain$top - hover$y) /
+    (hover$domain$top - hover$domain$bottom)) *
+    (hover$range$bottom - hover$range$top))
 
+  switch(
+    preferred_side,
 
-  # Use HTML/CSS to change style of tooltip panel here
+    "bottom_right" = {
+      if((hover$range$right - left_px) < minimum_h){
+        left_px <- (hover$range$right - minimum_h)}
+      if(((hover$range$top - top_px) + hover$range$bottom) < minimum_v){
+        top_px <- (hover$range$bottom - mimimum_v)}
+      return(paste0(
+        "position:absolute; z-index:100; background-color: ",
+        "rgba(", rgb[1], ", ", rgb[2], ", ", rgb[3], ", ", alpha, "); ",
+        "left:", left_px, "px; top:", top_px, "px;"))
+      },
 
-  style <- paste0(
-    "position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.85); ",
-    "left:", left_px + 2, "px; top:", top_px + 2, "px;")
+    "bottom_left" = {
+      if((left_px - hover$range$left) < minimum_h){
+        left_px <- hover$range$left + minimum_h}
+      if(((hover$range$top - top_px) + hover$range$bottom) < minimum_v){
+        top_px <- (hover$range$top - (hover$range$bottom + mimimum_v))}
+      return(paste0(
+        "position:absolute; z-index:100; background-color: ",
+        "rgba(", rgb[1], ", ", rgb[2], ", ", rgb[3], ", ", alpha, "); ",
+        "right:", hover$range$right - left_px, "px; top:", top_px, "px;"))
+      },
 
-  return(style)
+    "top_right" = {
+      if((hover$range$right - left_px) < minimum_h){
+        left_px <- (hover$range$right - minimum_h)}
+      if((top_px - hover$range$top) < minimum_v){
+        top_px <- (hover$range$top + mimimum_v)}
+      return(paste0(
+        "position:absolute; z-index:100; background-color: ",
+        "rgba(", rgb[1], ", ", rgb[2], ", ", rgb[3], ", ", alpha, "); ",
+        "left:", left_px, "px; bottom:", hover$range$top - top_px, "px;"))
+    },
+
+    "top_left" = {
+      if((left_px - hover$range$left) < minimum_h){
+        left_px <- hover$range$left + minimum_h}
+      if((top_px - hover$range$top) < minimum_v){
+        top_px <- (hover$range$top + mimimum_v)}
+
+    },
+    stop(paste0(
+      "preferred_side argument must be one of the following:\n",
+      "'bottom_right', 'bottom_left','top_right', 'top_left'\n"))
+  )
 }
+
 
